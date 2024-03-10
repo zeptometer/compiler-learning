223ddc741f29d07a66ecd16d12d5d7cdf7b7d5e7
Yuito Murase <yuito@acupof.coffee>
2024-03-10 10:25:30 +0900

    Test eval

--- main.rs
+++ main.rs
@@ -32,8 +32,26 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn eval_literal() {
+        assert_eq!(eval(&*int(1)), 1);
+        assert_eq!(eval(&*int(10)), 10);
+        assert_eq!(eval(&*int(-999)), -999);
+    }
+
+    #[test]
+    fn eval_add() {
+        assert_eq!(eval(&*add(int(1), int(2))), 3);
+        assert_eq!(eval(&*add(int(0), add(int(2), int(3)))), 5);
+        assert_eq!(eval(&*add(add(int(2), int(3)), int(-10))), -5);
+    }
+
+    #[test]
+    fn eval_mul() {
+        assert_eq!(eval(&*mul(int(1), int(2))), 2);
+        assert_eq!(eval(&*mul(int(2), add(int(2), int(3)))), 10);
+        assert_eq!(eval(&*mul(mul(int(2), int(3)), int(0))), 0);
     }
 }
