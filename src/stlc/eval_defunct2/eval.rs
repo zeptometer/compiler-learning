c0c0c06d8b6b63202711e4d800103fb69df9ffe4
Yuito Murase <yuito@acupof.coffee>
2024-04-29 15:59:48 +0900

    Complete eval_defunct2 (currying and defunctionalization)

--- src/stlc/eval_defunct2/eval.rs
+++ src/stlc/eval_defunct2/eval.rs
@@ -6,34 +6,34 @@ fn apply_cont(cont: Cont, v: Rc<Val>) -> Rc<Val> {
         Cont::Cont0 => v,
         Cont::EvalArg(arg, env, cont) => match &*v {
             Val::Clos(cenv, cbody) => {
-                (&arg.0(env, Cont::EvalClos(cbody.clone(), cenv.clone(), cont))).clone()
+                (apply_compt(arg, env, Cont::EvalClos(cbody.clone(), cenv.clone(), cont))).clone()
             }
             _ => val::error(),
         },
-        Cont::EvalClos(cbody, cenv, cont) => cbody.0(env::cons(v, cenv), *cont),
+        Cont::EvalClos(cbody, cenv, cont) => apply_compt(cbody, env::cons(v, cenv), *cont),
     }
 }
 
-pub fn eval(ast: Rc<Ast>) -> Compt {
-    match &*ast {
-        Ast::Int(i) => Compt(Rc::new(|env, cont| apply_cont(cont, Rc::new(Val::Int(*i))))),
-        Ast::Var(idx) => Compt(Rc::new(|env, cont| {
-            env::lookup(env, *idx)
-                .map(|v| apply_cont(cont, v))
-                .unwrap_or(val::error())
-        })),
-        Ast::Lam(body) => {
-            let body_compt = eval(body.clone());
-            Compt(Rc::new(|env, cont| {
-                apply_cont(cont, Rc::new(Val::Clos(env.clone(), body_compt)))
-            }))
+fn apply_compt(compt: Rc<Compt>, env: Rc<Env>, cont: Cont) -> Rc<Val> {
+    match &*compt {
+        Compt::Lit(i) => apply_cont(cont, val::int(*i)),
+        Compt::Access(idx) => env::lookup(env, *idx)
+            .map(|v| apply_cont(cont, v))
+            .unwrap_or(val::error()),
+        Compt::Close(body) => apply_cont(cont, val::clos(env.clone(), body.clone())),
+        Compt::Push(func, arg) => {
+            apply_compt(func.clone(), env.clone(), Cont::EvalArg(arg.clone(), env, Box::new(cont)))
         }
+    }
+}
+
+pub fn eval(ast: Rc<Ast>) -> Rc<Compt> {
+    match &*ast {
+        Ast::Int(i) => Rc::new(Compt::Lit(*i)),
+        Ast::Var(idx) => Rc::new(Compt::Access(*idx)),
+        Ast::Lam(body) => Rc::new(Compt::Close(eval(body.clone()))),
         Ast::App(func, arg) => {
-            let func_compt = eval(func.clone());
-            let arg_compt = eval(arg.clone());
-            Compt(Rc::new(|env, cont| {
-                func_compt.0(env.clone(), Cont::EvalArg(arg_compt, env, Box::new(cont)))
-            }))
+            Rc::new(Compt::Push(eval(func.clone()), eval(arg.clone())))
         }
     }
 }
@@ -50,15 +50,24 @@ mod tests {
         fn env1() -> Rc<Env> {
             cons(val::int(10), cons(val::int(20), empty()))
         }
-        assert_eq!(eval(ast::int(1), env1(), Cont::Cont0), val::int(1));
-        assert_eq!(eval(ast::var(0), env1(), Cont::Cont0), val::int(10));
-        assert_eq!(eval(ast::var(1), env1(), Cont::Cont0), val::int(20));
         assert_eq!(
-            eval(
-                ast::app(
+            apply_compt(eval(ast::int(1)), env1(), Cont::Cont0),
+            val::int(1)
+        );
+        assert_eq!(
+            apply_compt(eval(ast::var(0)), env1(), Cont::Cont0),
+            val::int(10)
+        );
+        assert_eq!(
+            apply_compt(eval(ast::var(1)), env1(), Cont::Cont0),
+            val::int(20)
+        );
+        assert_eq!(
+            apply_compt(
+                eval(ast::app(
                     ast::app(ast::lam(ast::lam(ast::var(1))), ast::int(33)),
                     ast::int(44)
-                ),
+                )),
                 env1(),
                 Cont::Cont0
             ),
